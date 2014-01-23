3dabab0ae52a673f0c07b000ac44d0ebfb8481a1
Jeffrey Way <jeffrey@envato.com>
2014-01-23 17:21:01 -0500

    Setup environments and db config

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'larademos',
-			'username'  => 'root',
-			'password'  => '1234',
+			'host'      => getenv('DB_HOST'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USER'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ app/config/local/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'larademos',
+            'username'  => 'root',
+            'password'  => '1234',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ]
+];
